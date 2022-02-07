71cd1ef665c3799829bd7648dcb853e09943ce44
James Kim <jamwjam@gmail.com>
2022-02-06 23:17:19 -0500

    Add heart svg

--- components/heartText/HeartText.tsx
+++ components/heartText/HeartText.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './heartText.module.css';
+import Image from 'next/image'
 
 /**
  * Primary UI component for user interaction
@@ -7,7 +8,7 @@ import styles from './heartText.module.css';
 export const HeartText = () => {
   return (
     <div className={styles.heartText}>
-      Made with Next.js and <span className={styles.heart}>❤️</span> by James
-    </div>
+      Made with Next.js and <Image className={styles.heart} src="/static/heart.svg" width="19.2px" height="16px" /> by James
+    </div >
   );
 };
