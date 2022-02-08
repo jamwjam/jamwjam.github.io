b1e12ac916c1ee13f32fe069bf40d9cd33696f9d
James Kim <jamwjam@gmail.com>
2022-02-08 01:12:56 -0500

    Fix apostrophe

--- pages/resources/index.tsx
+++ pages/resources/index.tsx
@@ -7,7 +7,7 @@ function Resources() {
             <ul>
                 <li><a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">A Complete Guide to Flexbox </a></li>
                 <li><a href="https://stackoverflow.com/questions/3197413/how-do-i-delete-unpushed-git-commits">How do I delete unpushed git commits?</a></li>
-                <li><a href="https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/">Var, Let, and Const – What's the Difference?</a></li>
+                <li><a href="https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/">Var, Let, and Const – What&#39;s the Difference?</a></li>
             </ul>
         </section>
     )
