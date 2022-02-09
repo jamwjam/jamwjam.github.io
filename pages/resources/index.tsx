723d23f72a69e9b6bf83a01ff412cd0b990d6f66
James Kim <jamwjam@gmail.com>
2022-02-09 00:29:45 -0500

    Fix quotes

--- pages/resources/index.tsx
+++ pages/resources/index.tsx
@@ -6,7 +6,7 @@ function Resources() {
             <p>just listing few links that I visit often for reference</p>
             <ul className={styles.list}>
                 <li><a href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">A Complete Guide to Flexbox </a></li>
-                <li><a href="https://jasonrudolph.com/blog/2009/02/25/git-tip-how-to-merge-specific-files-from-another-branch/">Git tip: How to "merge" specific files from another branch</a></li>
+                <li><a href="https://jasonrudolph.com/blog/2009/02/25/git-tip-how-to-merge-specific-files-from-another-branch/">Git tip: How to &#34;merge&#34; specific files from another branch</a></li>
                 <li><a href="https://stackoverflow.com/questions/3197413/how-do-i-delete-unpushed-git-commits">How do I delete unpushed git commits?</a></li>
                 <li><a href="https://www.freecodecamp.org/news/var-let-and-const-whats-the-difference/">Var, Let, and Const – What&#39;s the Difference?</a></li>
                 <li><a href="https://vercel.com/docs/concepts/functions/edge-functions">Vercel Edge Functions</a></li>
